995b8b5af79c5bc7d660e1f38f00818bd044edec
konstantin.bobkov <konstantin.bobkov@krainet.by>
2025-09-12 15:53:32 +0300

    feat: create project

---
+++ src/main/kotlin/com/laba/it_planner/ItPlannerApplication.kt
@@ -0,0 +1,11 @@
+package com.laba.it_planner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItPlannerApplication
+
+fun main(args: Array<String>) {
+    runApplication<ItPlannerApplication>(*args)
+}
---
+++ src/test/kotlin/com/laba/it_planner/ItPlannerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.laba.it_planner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItPlannerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
